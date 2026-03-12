7d20ea5b244fcf61730b7b4ec1dacfc2dc0ec291
Kamil Pietrzyk <s32529@pjwstk.edu.pl>
2026-03-12 23:20:07 +0100

    Initial project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using apbd_cw1_git_s32529.features;
+
+Console.WriteLine("Simple calculator!");
+Console.WriteLine("Please enter your input in the following format:");
+Console.WriteLine("[number_1]");
+Console.WriteLine("[operation ('+', '-', '*', '/')]");
+Console.WriteLine("[number_2]");
+
+double arg1 = double.Parse(Console.ReadLine());
+char operation = char.Parse(Console.ReadLine());
+double arg2 = double.Parse(Console.ReadLine());
+
+Calculator calc = new Calculator(arg1, arg2, operation);
+
+Console.WriteLine("Twój wynik to: " + calc.Result());
---
+++ features/Calculator.cs
@@ -0,0 +1,39 @@
+﻿namespace apbd_cw1_git_s32529.features;
+
+public class Calculator
+{
+    private double _arg1;
+    private double _arg2;
+    private double _operation;
+
+    public Calculator(double arg1, double arg2, char operation)
+    {
+        if (operation == '/' && arg2 == 0)
+        {
+            throw new Exception("Division by zero");
+        }
+        else
+        {
+            this._arg1 = arg1;
+            this._arg2 = arg2;
+            this._operation = operation;
+        }
+    }
+    
+    public double Result() 
+    {
+        switch (_operation)
+        {
+            case '+':
+                return _arg1 + _arg2;
+            case '-':
+                return _arg1 - _arg2;
+            case '*':
+                return _arg1 * _arg2;
+            case '/':
+                return _arg1 / _arg2;
+            default:
+                return 0;
+        }
+    }
+}
